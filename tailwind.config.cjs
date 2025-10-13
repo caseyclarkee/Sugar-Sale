d2a86e7671614cd9abab7fdb2ab77a402a09391a
caseyclarkee <casey.clarke@accenture.com>
2025-10-13 21:03:13 +1300

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,8 +3,27 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      /* Brand fonts (served by your Adobe link in index.html) */
+      fontFamily: {
+        futura: ['"futura-pt"', 'Futura', '"Avenir Next"', 'Avenir', 'sans-serif'],
+        futuraCondensed: [
+          '"futura-pt-condensed"',
+          '"Futura PT Condensed"',
+          '"futura-pt"',
+          'Futura',
+          'sans-serif',
+        ],
+      },
+
+      /* Optional semantic weights */
+      fontWeight: {
+        book: 400,
+        heavy: 900,
+      },
+
+      /* Your custom colours (simple names) */
       colors: {
-        purple: '#BB29BB',
+       purple: '#BB29BB',
         yellow: '#FFDD00',
         pink: '#f9a8d4',
         grey: '#333333',

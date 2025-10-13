0176a357bfb6a4a666714c73a20923753d781335
caseyclarkee <casey.clarke@accenture.com>
2025-10-13 18:13:43 +1300

    Delete public/src directory

--- public/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import SugarSaleSite from './App.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <SugarSaleSite />
-  </React.StrictMode>,
-)

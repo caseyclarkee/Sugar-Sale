8514d936d1e65179c3cbfdbac2c4b2b2e05616a4
caseyclarkee <casey.clarke@accenture.com>
2025-10-13 22:16:33 +1300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -270,27 +270,35 @@ export default function SugarSaleSite() {
             <footer className="border-t-[4px] border-grey bg-gray-100 py-8 w-full px-4 sm:px-8">
               <div className="w-full flex flex-col items-center justify-between gap-3 md:flex-row">
                 <p className="text-center text-sm font-medium md:text-left">
-                  © {new Date().getFullYear()} Long White — X Zero Sugar.
+                  © {new Date().getFullYear()} Long White X Zero Sugar.
                 </p>
                 <div className="flex items-center gap-3">
-                   <a
-                    href="https://www.asahibeverages.com/website-terms-of-use-new-zealand"
-                    className="rounded-lg border-[4px] border-grey bg-purple px-3 py-1 text-sm font-black uppercase text-white shadow-[3px_3px_0_#000]"
-                  >
-                    Website Terms of Use
-                  </a>
-                  <a
-                    href="https://www.asahibeverages.com/privacy-collection-notice"
-                    className="rounded-lg border-[4px] border-grey bg-yellow px-3 py-1 text-sm font-black uppercase shadow-[3px_3px_0_#000]"
-                  >
-                    Privacy Collection Notice
-                  </a>
-                  <a
-                    href="https://www.asahibeverages.com/privacy-policy"
-                    className="rounded-lg border-[4px] border-grey bg-purple px-3 py-1 text-sm font-black uppercase text-white shadow-[3px_3px_0_#000]"
-                  >
-                    Privacy Policy
-                  </a>
+                <a
+  href="https://www.asahibeverages.com/website-terms-of-use-new-zealand"
+  target="_blank"
+  rel="noopener noreferrer"
+  className="rounded-lg border-[4px] border-grey bg-purple px-3 py-1 text-sm font-black uppercase text-white shadow-[3px_3px_0_#000]"
+>
+  Website Terms of Use
+</a>
+
+<a
+  href="https://www.asahibeverages.com/privacy-collection-notice"
+  target="_blank"
+  rel="noopener noreferrer"
+  className="rounded-lg border-[4px] border-grey bg-yellow px-3 py-1 text-sm font-black uppercase shadow-[3px_3px_0_#000]"
+>
+  Privacy Collection Notice
+</a>
+
+<a
+  href="https://www.asahibeverages.com/privacy-policy"
+  target="_blank"
+  rel="noopener noreferrer"
+  className="rounded-lg border-[4px] border-grey bg-purple px-3 py-1 text-sm font-black uppercase text-white shadow-[3px_3px_0_#000]"
+>
+  Privacy Policy
+</a>
                 </div>
               </div>
             </footer>

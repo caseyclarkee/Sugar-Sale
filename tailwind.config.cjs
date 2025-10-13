5cc31d93ddb4445b970ffe7bf983a77f58d99c14
caseyclarkee <casey.clarke@accenture.com>
2025-10-13 20:43:25 +1300

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      colors: {
+        purple: '#BB29BB',
+        yellow: '#FFDD00',
+        pink: '#f9a8d4',
+        grey: '#333333',
+        black: '#000000',
+      },
+    },
+  },
   plugins: [],
 };

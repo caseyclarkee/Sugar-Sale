f7a26a2c7d920616d112ee4bbc43163a9591184e
caseyclarkee <casey.clarke@accenture.com>
2025-10-14 23:59:46 +1300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -57,24 +57,25 @@ const Header = () => (
   <header className="sticky top-0 z-50 grid gap-4 border-b-[4px] border-grey bg-white/95 backdrop-blur py-4 w-full overflow-visible">
     <div className="flex items-center justify-between w-full px-4 sm:px-8">
       <img
-        src="/images/sugar-lockup.png"
+  src="/images/gary.gif" // Use your desired image here
+  alt="THAT’S UNXPECTED"
+  onClick={() => {
+    const urls = [
+      "https://media3.giphy.com/media/VFZDuY0nePXry/giphy.gif",
+      "https://media4.giphy.com/media/gjgWQA5QBuBmUZahOP/giphy.gif",
+      "https://media1.giphy.com/media/8cEFp9dQCcE8M/giphy.gif",
+    ];
+    const randomUrl = urls[Math.floor(Math.random() * urls.length)];
+    window.open(randomUrl, "_blank");
+  }}
+  className="ml-6 inline-block rounded-full border-[4px] border-grey bg-purple px-3 sm:px-4 py-2 text-white font-black uppercase shadow-[6px_6px_0px_black] hover:scale-105 transition-transform cursor-pointer h-16 w-auto"
+/>
+      <img
+        src="/images/lockup.gif"
         alt="Sugar Liquidation Sale"
         className="h-28 sm:h-36 w-auto"
       />
-      <button
-        onClick={() => {
-          const urls = [
-            "https://media3.giphy.com/media/VFZDuY0nePXry/giphy.gif",
-            "https://media4.giphy.com/media/gjgWQA5QBuBmUZahOP/giphy.gif",
-            "https://media1.giphy.com/media/8cEFp9dQCcE8M/giphy.gif",
-          ];
-          const randomUrl = urls[Math.floor(Math.random() * urls.length)];
-          window.open(randomUrl, "_blank");
-        }}
-        className="ml-6 inline-block rounded-full border-[4px] border-grey bg-purple px-3 sm:px-4 py-2 text-white font-black uppercase shadow-[6px_6px_0px_black] hover:scale-105 transition-transform"
-      >
-        THAT’S UNXPECTED
-      </button>
+      <img src="/images/lwlogo.png" alt="X by Long White" className="h-full w-full object-cover" />
     </div>
 
     <nav className="w-full relative z-[60] py-2">

43dd9aae7b32fa72f9d075d1e5231b8e1854436b
caseyclarkee <casey.clarke@accenture.com>
2025-10-15 20:09:58 +1300

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const About = () => (
-  <div className="bg-[url(/about/images/clouds.jpg)] bg-cover bg-center">
+  <div className="bg-[url(images/about/clouds.jpg)] bg-cover bg-center">
     <section className="px-4 sm:px-8 py-12 text-lg">
       <h3 className="text-4xl sm:text-5xl font-black mb-4">About X</h3>
 

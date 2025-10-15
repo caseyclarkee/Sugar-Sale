8f1ffd9bd1f817df1c7685f1661567f23eab4e74
caseyclarkee <casey.clarke@accenture.com>
2025-10-16 01:25:37 +1300

    Update X.jsx

--- src/pages/X.jsx
+++ src/pages/X.jsx
@@ -9,51 +9,46 @@ const X = () => {
       className="bg-cover bg-center bg-no-repeat"
       style={{ backgroundImage: "url('/images/about/clouds.jpg')" }}
     >
-      <section className="px-6 sm:px-12 py-0 text-lg">
-        {/* 3-column layout: Gary | Text | Cans */}
-        <div className="grid grid-cols-12 gap-6 md:gap-10 items-center min-h-[80vh]">
-          {/* LEFT: Gary with minimal GIF lozenge */}
-          <div className="col-span-12 md:col-span-4 order-last md:order-1 relative flex justify-center md:justify-end">
+      <section className="px-6 sm:px-12 py-10 lg:py-16 text-lg">
+        <h3 className="text-4xl sm:text-5xl font-black mb-10 text-center md:text-left">
+          About X
+        </h3>
+
+        {/* 2-column layout: Gary + Text | Cans */}
+        <div className="grid grid-cols-12 gap-6 md:gap-8 items-end">
+          {/* LEFT: Gary + Text + GIF (text vertically centered, Gary static bottom) */}
+          <div className="col-span-12 md:col-span-6 order-2 md:order-1 flex flex-col items-center md:items-start text-center md:text-left relative">
+            <div className="flex flex-col justify-center h-full absolute inset-0 md:static">
+              <div className="max-w-md mx-auto md:mx-0">
+                <p className="leading-relaxed">
+                  <b>X skipped the sugar, but Gary’s got a plan. Grab a sweet deal, crack open a can.</b>
+                  <br />
+                  Forget everything you know about Long White. Introducing X by Long White.
+                  A refreshing Zero Sugar, Zero Carb, 94 Calories, Lemon Vodka flavoured RTD.
+                  <br />
+                  Expect the unXpected.
+                </p>
+                <img
+                  src={UnXpected}
+                  alt="THAT’S UNXPECTED"
+                  className="block max-h-[160px] w-auto object-contain mt-4"
+                />
+              </div>
+            </div>
+
             <img
               src={Gary}
               alt="Gary holding a can of X"
-              className="block w-auto max-h-[460px] sm:max-h-[520px] md:max-h-[560px] object-contain md:absolute md:bottom-0 md:left-0"
+              className="block w-auto max-h-[520px] md:self-start relative z-10"
             />
-
-            {/* Small GIF lozenge, no border or shadow */}
-            <div className="absolute right-[-10px] bottom-[20px] sm:right-[-20px] sm:bottom-[40px] md:right-[-40px] md:bottom-[60px]">
-              <img
-                src={UnXpected}
-                alt="THAT’S UNXPECTED"
-                className="block w-[60px] sm:w-[80px] md:w-[100px] h-auto object-contain rounded-full"
-              />
-            </div>
           </div>
 
-          {/* MIDDLE: Centered header + text */}
-          <div className="col-span-12 md:col-span-4 order-1 md:order-2 flex flex-col justify-center items-center text-center gap-4 md:gap-6">
-            <div className="flex flex-col items-center justify-center mb-1">
-              <h1 className="font-black tracking-tight leading-tight text-[clamp(38px,6.4vw,88px)]">
-                Introducing
-              </h1>
-              <p className="font-black tracking-tight leading-none text-[clamp(28px,4.6vw,56px)] -mt-1">
-                X by Long White
-              </p>
-            </div>
-
-            <p className="max-w-md leading-snug">
-              Forget everything you know about Long White. Introducing X by Long White. A refreshing Zero Sugar, Zero Carb, 94 Calories, Lemon Vodka flavoured RTD.
-              <br />
-              <b>“X skipped the sugar, but Gary’s got a plan. Grab a sweet deal, crack open a can.”</b>
-            </p>
-          </div>
-
-          {/* RIGHT: Can image */}
-          <div className="col-span-12 md:col-span-4 order-2 md:order-3 flex justify-center md:justify-start">
+          {/* RIGHT: Can image (unchanged position) */}
+          <div className="col-span-12 md:col-span-6 order-1 md:order-2 flex justify-center md:justify-end items-end">
             <img
               src={Can}
               alt="X by Long White cans"
-              className="block w-auto max-h-[520px] sm:max-h-[580px] md:max-h-[700px] object-contain"
+              className="block w-auto max-h-[500px] object-contain"
             />
           </div>
         </div>

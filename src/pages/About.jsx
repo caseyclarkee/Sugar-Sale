403b6044f22387864c5509cb592b931dae210081
caseyclarkee <casey.clarke@accenture.com>
2025-10-15 23:20:46 +1300

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -9,15 +9,25 @@ const About = () => {
       className="bg-cover bg-center bg-no-repeat"
       style={{ backgroundImage: "url('/images/about/clouds.jpg')" }}
     >
-      <section className="px-0 sm:px-0 py-0 text-lg backdrop-blur-sm">
-        <h3 className="mt-5 text-4xl sm:text-5xl font-black mb-5">About X</h3>
+      <section className="px-6 sm:px-12 py-10 lg:py-16 text-lg">
+        <h3 className="text-4xl sm:text-5xl font-black mb-10 text-center md:text-left">
+          About X
+        </h3>
 
-        {/* 2-column responsive layout */}
-        <div className="grid grid-cols-12 gap-0 items-center">
+        {/* 3-column layout: Gary | Text | Cans */}
+        <div className="grid grid-cols-12 gap-6 md:gap-8 items-end">
+          {/* LEFT: Gary (bottom aligned) */}
+          <div className="col-span-12 md:col-span-4 order-last md:order-1 relative flex justify-center md:justify-start">
+            <img
+              src={Gary}
+              alt="Gary holding a can of X"
+              className="block w-auto max-h-[520px] md:absolute md:bottom-0 md:left-0"
+            />
+          </div>
 
-          {/* Left: Text + GIF + Gary */}
-          <div className="col-span-12 md:col-span-5 order-2 md:order-1 flex flex-col items-center text-center">
-            <p className="max-w-md">
+          {/* MIDDLE: Text + GIF */}
+          <div className="col-span-12 md:col-span-4 order-1 md:order-2 flex flex-col items-center text-center md:items-center gap-5 md:gap-8">
+            <p className="max-w-md leading-relaxed">
               <b>X skipped the sugar, but Gary’s got a plan. Grab a sweet deal, crack open a can.</b>
               <br />
               Forget everything you know about Long White. Introducing X by Long White.
@@ -25,27 +35,22 @@ const About = () => {
               <br />
               Expect the unXpected.
             </p>
+
             <img
               src={UnXpected}
               alt="THAT’S UNXPECTED"
-              className="block max-h-[180px] w-auto object-contain"
+              className="block max-h-[160px] w-auto object-contain mt-2 md:mt-4"
             />
-            <img
-    src={Gary}
-    alt="Gary holding a can of X"
-    className="block max-h-auto w-auto object-contain align-bottom"
-  />
           </div>
 
           {/* RIGHT: Can image */}
-          <div className="col-span-12 md:col-span-7 order-1 md:order-2 flex justify-center">
+          <div className="col-span-12 md:col-span-4 order-2 md:order-3 flex justify-center md:justify-end">
             <img
               src={Can}
               alt="X by Long White cans"
-              className="block h-auto w-auto object-contain"
+              className="block w-auto max-h-[500px] object-contain"
             />
           </div>
-
         </div>
       </section>
     </div>

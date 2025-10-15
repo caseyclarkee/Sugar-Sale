32cdf0bbb9d3be7097c30d2684eed1d220203e2e
caseyclarkee <casey.clarke@accenture.com>
2025-10-15 17:32:45 +1300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -192,9 +192,10 @@ const LeftRail = () => {
   };
 
   return (
-    <aside
-      ref={railRef}
-      className="hidden md:flex fixed right-0 top-0 h-screen w-[140px] lg:w-[200px] border-l-[4px] border-grey bg-purple p-0 z-40"
+<aside
+  ref={railRef}
+  className="hidden md:flex fixed right-0 top-0 h-screen w-[140px] lg:w-[200px] border-l-[4px] border-grey bg-purple p-0 z-40"
+>
     >
       <div className="h-full w-full flex flex-col">
         {panels.map((p, i) => (

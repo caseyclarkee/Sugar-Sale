2d3991db07737cd8e233ddb43876aa5a54d19f81
caseyclarkee <casey.clarke@accenture.com>
2025-10-15 22:35:34 +1300

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Gary from "/images/about/PhotoGary.png";
 import Can from "/images/about/CanCallOuts.png";
+import UnXpected from "/images/gary.gif";
 
 const About = () => {
   return (
@@ -9,15 +10,20 @@ const About = () => {
       style={{ backgroundImage: "url('/images/about/clouds.jpg')" }}
     >
       <section className="px-0 sm:px-0 py-0 text-lg backdrop-blur-sm">
-        <h3 className="mt-1 text-4xl sm:text-5xl font-black mb-8">About X</h3>
+        <h3 className="mt-5 text-4xl sm:text-5xl font-black mb-5">About X</h3>
 
         {/* 3-column responsive layout */}
         <div className="grid grid-cols-12 gap-0 items-center">
           {/* LEFT: Text */}
           <div className="col-span-12 md:col-span-4 order-1 md:order-1 flex justify-center">
             <p className="max-w-md">
-              Forget everything you know about Long White. Introducing X by Long White. A refreshing Zero Sugar, Zero Carb, 94 Calories, Lemon Vodka flavoured RTD. Expect the unXpected.
+              <b>X Skipped the sugar, but Gary's got a plan. Grab a sweet deal, crack open a can.</b><br>Forget everything you know about Long White. Introducing X by Long White. A refreshing Zero Sugar, Zero Carb, 94 Calories, Lemon Vodka flavoured RTD. Expect the unXpected.</br>
             </p>
+            <img
+              src={Gary}
+              alt="Gary holding a can of X"
+              className="max-h-[450px] w-auto object-contain"
+            />
           </div>
 
           {/* RIGHT: Can image */}
@@ -25,7 +31,7 @@ const About = () => {
             <img
               src={Can}
               alt="X by Long White cans"
-              className="max-h-[400px] w-auto object-contain"
+              className="h-auto w-auto object-contain"
             />
           </div>
 

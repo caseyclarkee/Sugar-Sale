b6ce49b980520d47e3d7845209383b24b0844f54
caseyclarkee <casey.clarke@accenture.com>
2025-10-15 22:49:28 +1300

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -16,8 +16,8 @@ const About = () => {
         <div className="grid grid-cols-12 gap-0 items-center">
 
           {/* LEFT: Text + GIF */}
-          <div className="col-span-12 md:col-span-4 order-1 md:order-1 flex flex-col items-center md:items-start gap-4">
-            <p className="max-w-md">
+          <div className="col-span-12 md:col-span-5 order-1 md:order-1 flex flex-col items-center md:items-start gap-4">
+            <p className="max-w-md; text-center">
               <b>X skipped the sugar, but Gary’s got a plan. Grab a sweet deal, crack open a can.</b>
               <br />
               Forget everything you know about Long White. Introducing X by Long White.
@@ -33,7 +33,7 @@ const About = () => {
           </div>
 
           {/* RIGHT: Can image */}
-          <div className="col-span-12 md:col-span-4 order-2 md:order-3 flex justify-center">
+          <div className="col-span-12 md:col-span-5 order-2 md:order-3 flex justify-center">
             <img
               src={Can}
               alt="X by Long White cans"
@@ -42,7 +42,7 @@ const About = () => {
           </div>
 
           {/* MIDDLE: Gary (bottom on mobile) */}
-          <div className="col-span-12 md:col-span-4 order-last md:order-2 flex justify-center items-end">
+          <div className="col-span-12 md:col-span-2 order-last md:order-2 flex justify-center items-end">
             <img
               src={Gary}
               alt="Gary holding a can of X"

10c5ff598066f131f3dce240f847473f58d99f68
caseyclarkee <casey.clarke@accenture.com>
2025-10-18 20:58:58 +1300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,7 +19,7 @@ const Home = () => (
       <h2 className="text-4xl sm:text-5xl font-black uppercase mb-6 drop-shadow-[2px_2px_0_#000]">
         <img src="/images/x_website_header.gif"
           alt="Sugar Liquidation! Sale!"
-          className="w-md"></img>
+          className="w-5/6"></img>
       </h2>
 
       {/* Replaced Burst badge with a responsive PNG that stays attached to the card */}

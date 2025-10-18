866b38f4d15bba8943c1f81fb077521d68208297
caseyclarkee <casey.clarke@accenture.com>
2025-10-18 21:36:27 +1300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,7 +16,7 @@ const Burst = ({ children, className = "" }) => (
 const Home = () => (
   <section className="py-10 px-4 sm:px-8">
     <div className="relative w-full rounded-2xl border-[4px] border-grey bg-purple-300 shadow-[6px_6px_0_#000] p-6 sm:p-8 items-center text-center text-yellow">
-      <h2 className="text-4xl sm:text-5xl font-black uppercase mb-6 drop-shadow-[2px_2px_0_#000]">
+      <h2 className="text-4xl sm:text-5xl font-black uppercase mb-6 drop-shadow-[2px_2px_0_#000] text-center items-center ">
         <img src="/images/x_website_header.gif"
           alt="Sugar Liquidation Sale!"
           className="w-3/4 lg:w-1/2 center"></img>

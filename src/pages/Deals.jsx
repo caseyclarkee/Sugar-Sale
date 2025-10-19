50dad3d75e22964d19677bc99f8453eda2766850
caseyclarkee <casey.clarke@accenture.com>
2025-10-19 18:19:34 +1300

    Update Deals.jsx

--- src/pages/Deals.jsx
+++ src/pages/Deals.jsx
@@ -230,8 +230,8 @@ const DealCard = ({ deal }) => {
 function Deals() {
   // Hard-coded tiles
   const deals = [
-    { id: 1, title: "Sugar Dentures", image: "/images/dentures", ribbon: { text: "Sold Out", tone: "red" }, disabled: true, disabledLabel: "Sold Out" },
-    { id: 2, title: "10kg of Sugar", image: "/images/10kg", ribbon: { text: "Replenishing soon", tone: "purple" }, waitlist: true },
+    { id: 1, title: "Sugar Dentures", image: "/images/Sugar Dentures", ribbon: { text: "Sold Out", tone: "red" }, disabled: true, disabledLabel: "Sold Out" },
+    { id: 2, title: "10kg of Sugar", image: "/images/deals/Bag of Sugar", ribbon: { text: "Replenishing soon", tone: "purple" }, waitlist: true },
     { id: 3, title: "Deal of the Day", placeholder: true, badges: [{ text: "FREE!", tone: "blue" },{ text: "Giveaway", tone: "yellow" }] },
     { id: 4, title: "Deal of the Day", placeholder: true, badges: [{ text: "Now $0.00", tone: "blue" },{ text: "Giveaway", tone: "yellow" }] },
     { id: 5, title: "Deal of the Day", placeholder: true, badges: [{ text: "WIN FOR FREEEEE!", tone: "blue" },{ text: "Giveaway", tone: "yellow" }] },

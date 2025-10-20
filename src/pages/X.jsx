a36b6b6f1ef636e9b71265faf925cdb1c79eef4b
caseyclarkee <casey.clarke@accenture.com>
2025-10-20 14:41:21 +1300

    Update X.jsx

--- src/pages/X.jsx
+++ src/pages/X.jsx
@@ -51,7 +51,7 @@ const X = () => {
           </div>
 
           {/* RIGHT: Can image */}
-          <div className="col-span-12 lg:col-span-4 order-1 lg:order-3 flex justify-center md:justify-start">
+          <div className="col-span-12 lg:col-span-4 order-1 lg:order-3 flex justify-center lg:justify-end">
             <img
               src={Can}
               alt="X by Long White cans"

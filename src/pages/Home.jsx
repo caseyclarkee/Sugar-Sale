e7a8ebd3abf83157589ea546240b7bd4be7ebe98
caseyclarkee <casey.clarke@accenture.com>
2025-10-23 15:53:02 +1300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -27,7 +27,7 @@ const Home = () => (
       {/* VIDEO/GIF WRAPPER */}
       <div className="relative mx-auto mt-6 aspect-video w-full max-w-6xl overflow-visible rounded-xl border-[4px] border-grey shadow-[4px_4px_0_#000]">
         <iframe
-          src="https://vimeo.com/1129405293/a946fa6fc4"
+          src="https://player.vimeo.com/video/1129405293?h=a946fa6fc4"
           width="100%"
           height="100%"
           frameBorder="0"
